696b6bcc3cea1b1e62c0e24065d4ccf4342ea5e2
hp <starfive520@gmail.com>
2020-08-03 23:11:57 +0630

    second commit

--- app/src/main/java/com/example/layouttest/MainActivity.kt
+++ app/src/main/java/com/example/layouttest/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.layouttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test)
+        setContentView(R.layout.assignment5)
+    }
+    fun onDigit(view : View){
+        Toast.makeText(this,"Button works",Toast.LENGTH_SHORT).show()
     }
 }
